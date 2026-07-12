981b2c073da0202d06d4786b7a7ad615e2566b6d
s895025.jp3 <s895025.jp3@gmail.com>
2026-07-12 12:47:47 +0800

    Add: UART echoes full line (handles \r\n edge case)

--- Core/Src/app_uart_cmd.c
+++ Core/Src/app_uart_cmd.c
@@ -3,19 +3,51 @@
 #include "main.h"
 #include "app_uart_cmd.h"
 
-static uint8_t rx_buf[2];
+static uint8_t rx_buf[1];
 static uint8_t line_buf[64];
+static uint8_t line_len;
+static char temp_char;
 
 extern UART_HandleTypeDef huart2;
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) // "override:在中斷服務程式（ISR）裡，執行收 2 個字元" 2026/07/11 [ADD] by s895025.
-{
-  printf("Receive input char: %c%c\r\n", rx_buf[0], rx_buf[1]); 
-  HAL_UART_Receive_IT(huart, rx_buf, 2);
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) // "override:在中斷服務程式（ISR）裡，執行收字元" 2026/07/12 [ADD] by s895025.
+{                                                       // 資料真正'進來'的時間點，只有 callback 知道
+  HAL_UART_Receive_IT(huart, rx_buf, 1);
+  if (rx_buf[0] != '\r' && rx_buf[0] != '\n')
+  {
+    if (line_len >= 63)
+    {
+      line_buf[line_len] = '\0';
+    }
+    else
+    {
+      line_buf[line_len] = rx_buf[0];
+      line_len++;
+    }
+  }
+  else
+  {
+    if ((rx_buf[0] == '\r') || (rx_buf[0] == '\n')) // 判斷是否為行結尾符號
+    {
+      if (rx_buf[0] == '\r')
+        temp_char = rx_buf[0];
+
+      if (!(temp_char == '\r' && rx_buf[0] == '\n')) // Windows：送 \r\n，Linux：送 \n
+      {                                              // 處理連續送 \r\n 時，只有 \r 會印出內容，避免 \r\n 重複印出2次
+        printf("Receive input string: ");
+        for (int i = 0; i < line_len; i++)
+        {
+          printf("%c", line_buf[i]);
+        }
+        printf("\r\n");
+        line_len = 0;
+      }
+    }
+  }
 }
 
-void App_UART_Receive(void) // "啟動中斷式接收，收 2 個字元" 2026/07/12 [ADD] by s895025.
+void App_UART_Receive(void) // "啟動中斷式接收字元" 2026/07/12 [ADD] by s895025.
 {
-  HAL_UART_Receive_IT(&huart2, rx_buf, 2);
+  HAL_UART_Receive_IT(&huart2, rx_buf, 1);
 }
 
